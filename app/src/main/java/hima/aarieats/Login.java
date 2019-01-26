37293397709243126ebf95e69689e65028c5cc36
sathvik <sathvik@tatvait.com>
2019-01-26 17:11:29 +0530

    login screen

---
+++ app/src/main/java/hima/aarieats/Login.java
@@ -0,0 +1,38 @@
+package hima.aarieats;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Login extends AppCompatActivity {
+
+    private EditText userName;
+
+    private EditText password;
+
+    private Button loginBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userName = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToMainActivity();
+            }
+        });
+    }
+
+    private void goToMainActivity() {
+        Intent goToMainActivity = new Intent(Login.this,MainActivity.class);
+        startActivity(goToMainActivity);
+    }
+}
